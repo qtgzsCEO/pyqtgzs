075db051f6cdd4342fee9ec49e25d77688441047
文天琪 <155210824+qtgzsCEO@users.noreply.github.com>
2024-07-24 17:29:36 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='pyqtgzs',
+      version='0.1',
+      description='QinTian_QAQ',
+      url='qtgzsceo.github.io',
+      author='Ling',
+      author_email='qtgzs666@163.com',
+      license='MIT',
+      packages=['pyqtgzs'],
+      zip_safe=False)
